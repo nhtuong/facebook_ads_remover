3cd632680fd07e92c6c13d9e9734cd9e54100eca
Tuong H. Nguyen <nhtuong@gmail.com>
2015-02-03 10:15:25 +0100

    Version 1.2
    Version 1.2

---
+++ fb.ads.remover.js
@@ -0,0 +1,22 @@
+
+
+	  
+
+function main(){
+	var txt = "Sponsored";
+	$('.userContentWrapper').each(function(){
+       if($(this).text().toUpperCase().indexOf(txt.toUpperCase()) != -1){
+           $(this).remove();
+       }
+    });
+}
+
+
+window.onload=main;
+window.onscroll=main;
+	
+
+
+
+
+
